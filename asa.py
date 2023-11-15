13de79f85a876e36232d575be04d2d1705eeaf8e
Adilet2628 <adiletraimbekov269@gmail.com>
2023-11-15 18:04:15 +0600

    posledniy

--- asa.py
+++ asa.py
@@ -1,3 +1,5 @@
+dfghjkl;'
+';lkjhgfdfghjkl;';lkjhgfdsfgtyuiop[;l.,mnbgvfghjkl;'
 jrcfyf lfdfq hfcnfytvcz gjnjve xnj z xrdcnde. nj xnj ns yt cxfkbdf cj vyjq  z yt  vjue  e;t nthgtnm ndjt jnyjitybt rj vyt 
 z k.,l. nt,z  
 fq kjdt ....
